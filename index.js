2a64cca4cb28476e2641c5d3f10e9007edcf0754
John Park <jspark419@yahoo.com>
2020-04-01 20:11:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log('HELLO')
+  return 'HELLO'
 }
 function logWhisper(string) {
   console.log('hello')
